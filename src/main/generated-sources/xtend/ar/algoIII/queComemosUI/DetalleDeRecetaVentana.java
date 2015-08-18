f417e57df1c8507e5b0a3e62435ef89f1960b262
adrianbarbani <adrianbarbani@hotmail.com>
2015-08-18 20:29:14 -0300

    label

--- src/main/generated-sources/xtend/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.java
+++ src/main/generated-sources/xtend/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.java
@@ -1,13 +1,27 @@
 package ar.algoIII.queComemosUI;
 
+import ar.algo.adriba.tp1.Comida;
+import ar.algo.adriba.tp1.Publica;
 import ar.algo.adriba.tp1.Receta;
+import ar.algo.adriba.tp1.RecetaBuilder;
+import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtext.xbase.lib.Pure;
+import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.MainWindow;
+import org.uqbar.lacar.ui.model.ControlBuilder;
 
+@Accessors
 @SuppressWarnings("all")
 public class DetalleDeRecetaVentana extends MainWindow<Receta> {
+  private Receta pizza;
+  
+  private static Comida prepizza = new Comida(200, "prepizza", 50);
+  
   public DetalleDeRecetaVentana() {
-    super(new Receta());
+    super(new RecetaBuilder().tipoDeReceta(new Publica()).nombreDelPlato("Pizza de muzzarella").agregarIngrediente(DetalleDeRecetaVentana.prepizza).setearCalorias(500).build());
+    Receta _modelObject = this.getModelObject();
+    this.pizza = _modelObject;
   }
   
   public static void main(final String[] args) {
@@ -17,5 +31,16 @@ public class DetalleDeRecetaVentana extends MainWindow<Receta> {
   
   public void createContents(final Panel mainPanel) {
     this.setTitle("Detalle De Receta");
+    Label _label = new Label(mainPanel);
+    _label.<Object, ControlBuilder>bindValueToProperty("nombreDelPlato");
+  }
+  
+  @Pure
+  public Receta getPizza() {
+    return this.pizza;
+  }
+  
+  public void setPizza(final Receta pizza) {
+    this.pizza = pizza;
   }
 }
--- src/main/java/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.xtend
+++ src/main/java/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.xtend
@@ -3,11 +3,26 @@ package ar.algoIII.queComemosUI
 import ar.algo.adriba.tp1.Receta
 import org.uqbar.arena.windows.MainWindow
 import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.widgets.TextBox
+import ar.algo.adriba.tp1.RecetaBuilder
+import ar.algo.adriba.tp1.Publica
+import ar.algo.adriba.tp1.Comida
+import org.eclipse.xtend.lib.annotations.Accessors
+import org.uqbar.commons.utils.Observable
+import org.uqbar.arena.widgets.Label
+
+@Accessors
 
 class DetalleDeRecetaVentana extends MainWindow<Receta> {
+	Receta pizza
+	static Comida prepizza = new Comida(200, "prepizza", 50)
+
 	
 	new(){
-		super(new Receta)
+		super(new RecetaBuilder().tipoDeReceta(new Publica).nombreDelPlato("Pizza de muzzarella").
+			agregarIngrediente(prepizza).setearCalorias(500).
+			build)
+		this.pizza = this.modelObject 	
 	}
 	
 	def static main (String []args){
@@ -16,6 +31,7 @@ class DetalleDeRecetaVentana extends MainWindow<Receta> {
 	
 	override createContents(Panel mainPanel) {
 			this.title = "Detalle De Receta"
+			new Label(mainPanel).bindValueToProperty("nombreDelPlato")
 	}
 	
 	
