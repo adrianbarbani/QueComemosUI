b76c3221d5eb4e4ba38a187ffbacc0fd6e7a5ce6
ArcticMonkeys <kromago@gmail.com>
2015-08-19 02:23:48 -0300

    Arme toda una base de la ventana, falta lo que explique Dodino el jueves

--- src/main/generated-sources/xtend/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.java
+++ src/main/generated-sources/xtend/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.java
@@ -5,7 +5,11 @@ import ar.algo.adriba.tp1.Publica;
 import ar.algo.adriba.tp1.Receta;
 import ar.algo.adriba.tp1.RecetaBuilder;
 import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 import org.eclipse.xtext.xbase.lib.Pure;
+import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.MainWindow;
@@ -20,7 +24,7 @@ public class DetalleDeRecetaVentana extends MainWindow<Receta> {
   
   public DetalleDeRecetaVentana() {
     super(
-      new RecetaBuilder().tipoDeReceta(new Publica()).nombreDelPlato("Pizza de muzzarella").agregarIngrediente(DetalleDeRecetaVentana.prepizza).setearCalorias(500).build());
+      new RecetaBuilder().tipoDeReceta(new Publica()).nombreDelPlato("Pizza de muzzarella").agregarIngrediente(DetalleDeRecetaVentana.prepizza).setearCalorias(500).setearDificultad("Facil").setearTemporadas("Todo el año").build());
     Receta _modelObject = this.getModelObject();
     this.pizza = _modelObject;
   }
@@ -34,8 +38,57 @@ public class DetalleDeRecetaVentana extends MainWindow<Receta> {
     this.setTitle("Detalle De Receta");
     Label _label = new Label(mainPanel);
     _label.<Object, ControlBuilder>bindValueToProperty("nombreDelPlato");
-    Label _label_1 = new Label(mainPanel);
+    final Panel panelCaloriasYDueño = new Panel(mainPanel);
+    HorizontalLayout _horizontalLayout = new HorizontalLayout();
+    panelCaloriasYDueño.setLayout(_horizontalLayout);
+    Label _label_1 = new Label(panelCaloriasYDueño);
     _label_1.<Object, ControlBuilder>bindValueToProperty("caloriasReceta");
+    Label _label_2 = new Label(panelCaloriasYDueño);
+    _label_2.setText("calorias");
+    final Panel panelDificultadYTemporada = new Panel(mainPanel);
+    HorizontalLayout _horizontalLayout_1 = new HorizontalLayout();
+    panelDificultadYTemporada.setLayout(_horizontalLayout_1);
+    final Panel panelDificultad = new Panel(panelDificultadYTemporada);
+    final Panel panelTemporada = new Panel(panelDificultadYTemporada);
+    Label _label_3 = new Label(panelDificultad);
+    _label_3.setText("Dificultad");
+    Label _label_4 = new Label(panelDificultad);
+    _label_4.<Object, ControlBuilder>bindValueToProperty("dificultad");
+    Label _label_5 = new Label(panelTemporada);
+    _label_5.setText("Temporada");
+    Label _label_6 = new Label(panelTemporada);
+    _label_6.<Object, ControlBuilder>bindValueToProperty("temporada");
+    final Panel panelIngredientesYCondimentos = new Panel(mainPanel);
+    HorizontalLayout _horizontalLayout_2 = new HorizontalLayout();
+    panelIngredientesYCondimentos.setLayout(_horizontalLayout_2);
+    final Panel panelIngredientes = new Panel(panelIngredientesYCondimentos);
+    final Panel panelCondimentos = new Panel(panelIngredientesYCondimentos);
+    Label _label_7 = new Label(panelIngredientes);
+    _label_7.setText("Ingredientes");
+    Label _label_8 = new Label(panelCondimentos);
+    _label_8.setText("Condimentos");
+    final Panel panelFavoritaYCondiciones = new Panel(mainPanel);
+    HorizontalLayout _horizontalLayout_3 = new HorizontalLayout();
+    panelFavoritaYCondiciones.setLayout(_horizontalLayout_3);
+    final Panel panelFavorita = new Panel(panelFavoritaYCondiciones);
+    HorizontalLayout _horizontalLayout_4 = new HorizontalLayout();
+    panelFavorita.setLayout(_horizontalLayout_4);
+    final Panel panelCondiciones = new Panel(panelFavoritaYCondiciones);
+    Label _label_9 = new Label(panelFavorita);
+    _label_9.setText("Favorita");
+    Label _label_10 = new Label(panelCondiciones);
+    _label_10.setText("Condiciones Preexistentes");
+    final Panel panelProcesoDePreparacion = new Panel(mainPanel);
+    Label _label_11 = new Label(panelProcesoDePreparacion);
+    _label_11.<Object, ControlBuilder>bindValueToProperty("explicacionDeLaPreparacion");
+    final Panel panelBotonVolver = new Panel(mainPanel);
+    Button _button = new Button(panelBotonVolver);
+    final Procedure1<Button> _function = new Procedure1<Button>() {
+      public void apply(final Button it) {
+        it.setCaption("Volver");
+      }
+    };
+    ObjectExtensions.<Button>operator_doubleArrow(_button, _function);
   }
   
   @Pure
--- src/main/java/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.xtend
+++ src/main/java/ar/algoIII/queComemosUI/DetalleDeRecetaVentana.xtend
@@ -11,6 +11,7 @@ import org.eclipse.xtend.lib.annotations.Accessors
 import org.uqbar.commons.utils.Observable
 import org.uqbar.arena.widgets.Label
 import org.uqbar.arena.layout.HorizontalLayout
+import org.uqbar.arena.widgets.Button
 
 @Accessors
 //-Djava.system.class.loader=com.uqbar.apo.APOClassLoader
@@ -21,7 +22,7 @@ class DetalleDeRecetaVentana extends MainWindow<Receta> {
 	new() {
 		super(
 			new RecetaBuilder().tipoDeReceta(new Publica).nombreDelPlato("Pizza de muzzarella").
-				agregarIngrediente(prepizza).setearCalorias(500).build)
+				agregarIngrediente(prepizza).setearCalorias(500).setearDificultad("Facil").setearTemporadas("Todo el año").build)
 		this.pizza = this.modelObject
 	}
 
@@ -29,11 +30,70 @@ class DetalleDeRecetaVentana extends MainWindow<Receta> {
 		new DetalleDeRecetaVentana().startApplication
 	}
 
-	override createContents(Panel mainPanel) {
+	override createContents(Panel mainPanel) { //Panel principal
 		this.title = "Detalle De Receta"
 		new Label(mainPanel).bindValueToProperty("nombreDelPlato")
-		new Label(mainPanel).bindValueToProperty("caloriasReceta")
-
+		
+		//------------------------------------------------------------------------
+		
+		val panelCaloriasYDueño = new Panel (mainPanel) //Panel que tiene una sola linea
+		panelCaloriasYDueño.layout = new HorizontalLayout
+		new Label(panelCaloriasYDueño).bindValueToProperty("caloriasReceta")
+		new Label(panelCaloriasYDueño).text = "calorias"
+		
+		//Falta ver el tema del dueño (en este mismo panel)
+		
+		//------------------------------------------------------------------------
+		
+		val panelDificultadYTemporada = new Panel (mainPanel) //Hago un panel grande que sea horizontal
+		panelDificultadYTemporada.layout = new HorizontalLayout
+		
+		val panelDificultad = new Panel (panelDificultadYTemporada) //Ese mismo panel lo divido en dos verticales
+		val panelTemporada = new Panel (panelDificultadYTemporada)
+		
+		new Label(panelDificultad).text = "Dificultad"
+		new Label(panelDificultad).bindValueToProperty("dificultad")
+		new Label(panelTemporada).text = "Temporada"
+		new Label(panelTemporada).bindValueToProperty("temporada")
+		
+		//------------------------------------------------------------------------
+		
+		val panelIngredientesYCondimentos = new Panel (mainPanel) //Repito el proceso de antes
+		panelIngredientesYCondimentos.layout = new HorizontalLayout
+		val panelIngredientes = new Panel (panelIngredientesYCondimentos)
+		val panelCondimentos = new Panel (panelIngredientesYCondimentos)
+		
+		new Label(panelIngredientes).text = "Ingredientes"
+		//aca hay que hacer la tabla
+		new Label(panelCondimentos).text = "Condimentos"
+		//aca hay que hacer la lista
+		
+		//------------------------------------------------------------------------
+		
+		val panelFavoritaYCondiciones = new Panel (mainPanel)	//Repito el proceso de antes
+		panelFavoritaYCondiciones.layout = new HorizontalLayout
+		val panelFavorita = new Panel (panelFavoritaYCondiciones)
+		panelFavorita.layout = new HorizontalLayout //Lo hago horizontal asi el cosito para marcar y la palabra favorita quedan juntos
+		val panelCondiciones = new Panel (panelFavoritaYCondiciones)
+		
+		new Label(panelFavorita).text = "Favorita"
+		//aca hay que hacer el cosito para marcar
+		new Label(panelCondiciones).text = "Condiciones Preexistentes"
+		//aca hay que hacer la lista
+		
+		//------------------------------------------------------------------------
+		
+		val panelProcesoDePreparacion = new Panel (mainPanel)	//Repito el proceso de antes
+		new Label(panelProcesoDePreparacion).bindValueToProperty("explicacionDeLaPreparacion") //agregar en el builder	
+		
+		//------------------------------------------------------------------------
+		
+		val panelBotonVolver = new Panel (mainPanel)
+		new Button(panelBotonVolver) =>
+			[
+				//onClick [ | ] aca hay que ver que hace cuando lo tocamos
+				caption = "Volver"
+			] 
 	}
 
 
